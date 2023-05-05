1fa2208c15c08ca84628689e27f8b433e1b2cd06
AliceyXia <xiay424@newschool.edu>
2023-05-05 09:15:35 -0400

    deleted the data js

--- nyc_data/assets/data.js
+++
@@ -1,7 +0,0 @@
-const dataUrl ='https://data.cityofnewyork.us/resource/nu7n-tubp.json'
-
-fetch(dataUrl +'?$limit=9999999&BreedName=Golden Retriever')
-	.then(response => response.json ())
-	.then(data => {
-			console.log(data)
-	})
